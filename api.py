1fc6b6a6d63be39930ec3725b32e4a1f7fd8ff2e
bhumikads2211-pixel <bhumikads2211@gmail.com>
2025-09-28 19:30:02 +0530

    Update api.py

--- api.py
+++ api.py
@@ -3,6 +3,5 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loginmodules.settings')
 
-def handle(event, context):
-    application = get_wsgi_application()
-    return application(event, context)
+app = get_wsgi_application()
+handler = app  # expose 'handler' as expected by Vercel
