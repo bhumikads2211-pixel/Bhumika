17d16bc39123b2c3b913735fbd389ae5c44212a2
bhumikads2211-pixel <bhumikads2211@gmail.com>
2025-09-28 19:28:28 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,8 @@
-from django.core.wsgi import get_wsgi_application
-from api import handle
 import os
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loginmodules.settings')
 
-application = get_wsgi_application()
-app = handle(application)
+def handle(event, context):
+    application = get_wsgi_application()
+    return application(event, context)
