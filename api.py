59b5928764bfed3bdabc25847671bd9ae55fcb7b
bhumika <bhumikads2211@gmail.com>
2025-09-28 19:00:41 +0530

    Vercel Deployement

--- api.py
+++ api.py
@@ -2,7 +2,6 @@ from django.core.wsgi import get_wsgi_application
 from vercel_wsgi import handle
 import os
 
-# ⛳ Replace 'your_project_name' with your actual project name
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loginmodules.settings')
 
 application = get_wsgi_application()
